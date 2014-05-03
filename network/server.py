02ebf9de8f9acfcb8d6e2b51a8da262a4b1786bb
Zachary Robert Lipp <zlipp@fitz87.helios.nd.edu>
2014-05-03 17:48:05 -0400

    network last of the day

--- network/server.py
+++ network/server.py
@@ -1,10 +1,11 @@
-#client file
+#server
 from twisted.internet import reactor, protocol
 import sys
 from twisted.internet.defer import DeferredQueue
 from ../evolution import GameSpace
 import pickle
 from twisted.internet.task import LoopingCall
+
 
 #protocol for server and client
 class Prot(protocol.Protocol):
@@ -15,57 +16,57 @@ class Prot(protocol.Protocol):
     def dataReceived(self, data):#when data received
         recv = pickle.loads(data)#load game state and set game state
         
-        #adjust game state set comp and other player position
-        self.game.enemy = recv['player']
-        self.game.comps = recv['comps']
+        #adjust other player
+        self.game.enemy = recv
 
     def connectionMade(self):
         print "Connection made"
+        #create loopingCall
+        self.lc =LoopingCall(self.game.loop_iteration,None, None)
+        self.lc.start(1/60)
+
         self.q.get().addCallback(self.ForwardData)
 
     def connectionLost(self, reason):
         #close game
+        lc.stop()
         print "Error: connection lost"
 
     #writes data in q and prepares for next item
     def ForwardData(self, data):
         try:
             #send player and computer array
-            toSend = self.game.player
+            toSend = dict()
+            toSend['player'] = self.game.player
+            toSend['fish'] = self.game.fish
 
-            w = pickle.dumps(toSend) #serialize game state
+            w = pickle.dumps(toSend) #serialize 
             self.transport.write(w) #send game state
             self.q.get().addCallback(self.ForwardData)
         except Exception as ex:
             print "Failed to send game state: "
             self.transport.write("Failed to get update")
 
-
 #factory to create connection
-class Client(protocol.ClientFactory):
-    def __init__(self, host, port):       
-        self.q = DeferredQueue()
-
+class Server(protocol.Factory):
+    def __init__(self):
+        self.q = DeferredQueue() 
+        
         #create game and set as self.game
-        self.game = GameSpace(q) # every time game is change call q.put()
-
-        #create loopingCall
-        self.lc =LoopingCall(self.game.loop_iteration,None, None)
-        self.lc.start(1/60)
+        self.game = GameSpace(q);# every time game is change call q.put()
 
     def buildProtocol(self, addr):    
-        return Prot(self.q, self.game )
+        return Prot(self.q, self.game)
 
     def clientConnectionFailed(self, connector, reason):
-        print "Connection failed: check that server is running"
-        self.lc.stop()
-        reactor.stop()
+        print "Connection failed"
 
-#pass hostname and port in commandline
+#pass port in commandline
 if __name__=='__main__':
-    if sys.argc != 3:
-        print 'usage: python client.py <hostname> <port>'
+    if sys.argc != 2:
+        print 'usage: python client.py <port>'
     else:
-        f = Client(hostname, port)
-        reactor.connectTCP(host, int(port), f)
+        f=Server()
+        listenTCP(sys.arv[1], sys.argv[2], f)
         reactor.run()
+
